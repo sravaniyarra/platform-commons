a469ea024c4012c5a48e7cf0c098f1552c91e7c8
sravaniyarra <sravaniyarra55@gmail.com>
2025-02-17 18:35:15 +0530

    Changes made in services

--- src/app/shared/event.service.ts
+++ src/app/shared/event.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Events } from '../models/event.model';
 
 @Injectable({
@@ -9,44 +9,21 @@ import { Events } from '../models/event.model';
 export class EventService {
 
   constructor(private http : HttpClient) { }
-  private eventsSubject = new BehaviorSubject<Events[]>([]);  // Holds events list
-  events$ = this.eventsSubject.asObservable();  
   // private apiUrl = 'https://sravaniyarra.github.io/dbJson/db.json';
-  private apiUrl ='http://localhost:3000/events'
+  // private apiUrl ='http://localhost:3000/events'
+   private apiUrl = 'https://platform-commons-api-7ee5639348b6.herokuapp.com/events'
   
   //Get All Events
-  //  getAllEvents(): Observable<Events[]>{
-  //      return this.http.get<Events[]>(this.apiUrl);
-  //  }
+   getAllEvents(): Observable<Events[]>{
+       return this.http.get<Events[]>(this.apiUrl);
+   }
 
-  getAllEvents(){
-    this.http.get<Events[]>(this.apiUrl).subscribe(
-      (events) => {
-        this.eventsSubject.next(events);  // Update the BehaviorSubject with the new event list
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
- 
 
-  postEvent(newEvent: Events){
-    this.http.post<Events>(this.apiUrl, newEvent).subscribe(
-      (event) => {
-        const updatedEvents = [...this.eventsSubject.value, event];
-        this.eventsSubject.next(updatedEvents);  // Update the event list
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
- 
+   
    // Post Event
-  //  postEvent(id:any):Observable<Events>{
-  //      return this.http.post<Events>(this.apiUrl,id)
-  //  }
+   postEvent(id:any):Observable<Events>{
+       return this.http.post<Events>(this.apiUrl,id)
+   }
 
    //Get single event
 
@@ -56,38 +33,12 @@ export class EventService {
 
    //Update event
 
-  //  updateEvent(event:any,id:number):Observable<Events>{
-  //      return this.http.put<Events>(`${this.apiUrl}/${id}`,event)
-  //  }
-
-  updateEvent(updatedEvent: Events) {
-    this.http.put<Event>(`${this.apiUrl}/${updatedEvent.id}`, updatedEvent).subscribe(
-      () => {
-        const updatedEvents = this.eventsSubject.value.map((event) =>
-          event.id === updatedEvent.id ? updatedEvent : event
-        );
-        this.eventsSubject.next(updatedEvents);  // Update the event list
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
+   updateEvent(event:any,id:number):Observable<Events>{
+       return this.http.put<Events>(`${this.apiUrl}/${id}`,event)
+   }
  // Delete event
  
-  // deleteEvent(id:number):Observable<Events>{
-  // return this.http.delete<Events>(`${this.apiUrl}/${id}`)
-  //  }
-
-  deleteEvent(eventId: number) {
-    this.http.delete(`${this.apiUrl}/${eventId}`).subscribe(
-      () => {
-        const updatedEvents = this.eventsSubject.value.filter((event) => event.id !== eventId);
-        this.eventsSubject.next(updatedEvents);  // Remove the deleted event
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
+  deleteEvent(id:number):Observable<Events>{
+  return this.http.delete<Events>(`${this.apiUrl}/${id}`)
+   }
 }
