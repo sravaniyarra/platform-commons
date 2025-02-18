b56997a00a7a9c4e6533993a75adee5874eef82d
sravaniyarra <sravaniyarra55@gmail.com>
2025-02-18 13:41:35 +0530

    auth guard created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { EventDetailsComponent } from './event-details/event-details.component';
 import { EventFormComponent } from './event-form/event-form.component';
+import { AuthGuard } from './authGuard/auth.guard';
 
 const routes: Routes = [
   {path:'',redirectTo:'login',pathMatch:'full'},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegistrationComponent},
-  {path : 'event-details',component:EventDetailsComponent},
+  {path : 'event-details',component:EventDetailsComponent,canActivate: [AuthGuard] },
   {path : 'create-event',component:EventDetailsComponent},
   {path:'event-form',component:EventFormComponent}
 ];
